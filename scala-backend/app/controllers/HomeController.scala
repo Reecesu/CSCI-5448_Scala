265c44821590368239b0e2a487f796d399e24eea
Reece Suchocki <76573091+Reecesu@users.noreply.github.com>
2023-04-10 04:13:18 -0600

    initial lettucewrap commit

---
+++ scala-backend/app/controllers/HomeController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json.Json
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def evaluate(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  // Placeholder response
+  Ok(Json.obj("result" -> "Your Scala backend is connected"))
+}
+}
